bde7027f42f81c281783f9bd64d36045aa932786
Felipe Miranda <feguido1@gmail.com>
2019-08-28 00:52:56 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/mirandox/cursomc/services/CategoriaService.java
+++ src/main/java/com/mirandox/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.mirandox.cursomc.domain.Categoria;
 import com.mirandox.cursomc.repositories.CategoriaRepository;
+import com.mirandox.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,8 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> categoria = categoriaRepository.findById(id);
-		return categoria.orElse(null);
+		
+		return categoria.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
