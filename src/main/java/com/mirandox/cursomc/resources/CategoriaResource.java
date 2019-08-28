b825d46168672276550a49448377f7ffb0c97ea2
Felipe Miranda <feguido1@gmail.com>
2019-08-28 15:47:18 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/mirandox/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/mirandox/cursomc/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.mirandox.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mirandox.cursomc.domain.Categoria;
 import com.mirandox.cursomc.services.CategoriaService;
@@ -15,13 +19,24 @@ import com.mirandox.cursomc.services.CategoriaService;
 public class CategoriaResource {
 	
 	@Autowired
-	private CategoriaService categoriaSerivce;
+	private CategoriaService categoriaService;
 
-	@RequestMapping(value= "/{id}", method=RequestMethod.GET)
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
-		Categoria categoria = categoriaSerivce.buscar(id);
+		Categoria categoria = categoriaService.buscar(id);
 		
 		return ResponseEntity.ok().body(categoria);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria categoria) {
+		categoria = categoriaService.insert(categoria);
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(categoria.getId())
+				.toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
