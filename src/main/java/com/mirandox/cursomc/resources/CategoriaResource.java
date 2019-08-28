364bedb1cbc4b8ed6c83fdc800478f547b297719
Felipe Miranda <feguido1@gmail.com>
2019-08-28 15:57:21 -0300

    Atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/com/mirandox/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/mirandox/cursomc/resources/CategoriaResource.java
@@ -22,10 +22,8 @@ public class CategoriaResource {
 	private CategoriaService categoriaService;
 
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
-		
-		Categoria categoria = categoriaService.buscar(id);
-		
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
+		Categoria categoria = categoriaService.find(id);
 		return ResponseEntity.ok().body(categoria);
 	}
 	
@@ -38,5 +36,12 @@ public class CategoriaResource {
 				.buildAndExpand(categoria.getId())
 				.toUri();
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Categoria categoria, @PathVariable Integer id) {
+		categoria.setId(id);
+		categoria = categoriaService.update(categoria);
+		return ResponseEntity.noContent().build();
 	}
 }
