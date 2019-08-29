d4de9432ed6d0db82497c1202d286487a6c5a439
Felipe Miranda <feguido1@gmail.com>
2019-08-29 11:38:28 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/mirandox/cursomc/services/CategoriaService.java
+++ src/main/java/com/mirandox/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.mirandox.cursomc.domain.Categoria;
+import com.mirandox.cursomc.dto.CategoriaDTO;
 import com.mirandox.cursomc.repositories.CategoriaRepository;
 import com.mirandox.cursomc.services.exceptions.DataIntegrityException;
 import com.mirandox.cursomc.services.exceptions.ObjectNotFoundException;
@@ -56,5 +57,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String direction, String orderBy) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy); 
 		return categoriaRepository.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO categoriaDTO) {
+		return new Categoria(categoriaDTO.getId(),categoriaDTO.getNome());
 	}
 }
